5a10417c992bad85de90f379d9a8e257080f32db
Miguel Jones <mjcoqui@gmail.com>
2022-07-05 22:51:51 +0000

    Working api key framework with get request

--- shuffle.py
+++ shuffle.py
@@ -1,6 +1,22 @@
+import requests
+import os
 
-def function1(num):
-    pass
+# Constants to get api key from environment variables
+API_KEY = os.environ.get('API_KEY')
+USER_AGENT = "user"
+# Root url of the api
+ROOT_URL = 'http://ws.audioscrobbler.com/2.0/'
 
-def function2(num1, num2):
-    pass
+headers = {
+    'user-agent': USER_AGENT
+}
+# Parameters that need to be passed to the get request
+payload = {
+    'method': 'chart.gettoptags',
+    'api_key': API_KEY,
+    'format': 'json'
+}
+
+r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+print(r.status_code)
+print(r.json())
