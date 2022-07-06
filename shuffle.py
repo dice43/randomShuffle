e97b364bae56400b335c64dba3ba02a6134e42de
Miguel Jones <mjcoqui@gmail.com>
2022-07-06 18:24:40 +0000

    Functional playlist generation with user input for genre

--- shuffle.py
+++ shuffle.py
@@ -12,11 +12,32 @@ headers = {
 }
 # Parameters that need to be passed to the get request
 payload = {
-    'method': 'chart.gettoptags',
+    'method': 'tag.gettoptags',
     'api_key': API_KEY,
     'format': 'json'
 }
-
+# Request to get a list of genres to choose from
 r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
-print(r.status_code)
-print(r.json())
+data = r.json()
+genresAvailable = []
+songs = []
+
+for tag in data['toptags']['tag']:
+   genresAvailable.append(tag['name'])
+print(f'These are the genres to choose from: {genresAvailable}')
+
+# Input from user picking which genre they would like in the playlist
+desiredGenre = input('Please enter a specific genre that you want the playlist to have: ')
+print('')
+parameters = {
+    'method': 'tag.gettoptracks',
+    'api_key': API_KEY,
+    'format': 'json',
+    'tag': desiredGenre
+}
+# Request that gets the top songs that match the chosen genre
+topSongs = requests.get(ROOT_URL, headers=headers, params = parameters).json()
+playlist = []
+for song in topSongs['tracks']['track']:
+    playlist.append(song['name'])
+print(f'The playlist is: {playlist}')
