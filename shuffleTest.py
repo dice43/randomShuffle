65a2889e9a1773331d66b71515f8f152d516838d
Miguel Jones <mjcoqui@gmail.com>
2022-07-07 19:50:44 +0000

    Updated unittest

--- shuffleTest.py
+++ shuffleTest.py
@@ -1,14 +1,38 @@
 import unittest
-from shuffle import function1, function2
+import os
+import pandas as pd
+import sqlalchemy as df
+import requests
+from shuffle import search_artist
 
 
-class TestFileName(unittest.TestCase):
-    def test_function1(self):
-        self.assertEqual(function1(1), 0)
+class ShuffleTest(unittest.TestCase):
 
-    def test_function2(self):
-        self.assertEqual(function2(2,1), 3)
-        self.assertEqual(function2(2.1, 1.2), 3.3)
+    def setUp(self):
+        self.API_KEY = os.environ.get('API_KEY')
+        self.USER_AGENT = "user"
+        # Root url of the api
+        self.ROOT_URL = 'http://ws.audioscrobbler.com/2.0/'
+
+        self.headers = {
+            'user-agent': self.USER_AGENT
+        }
+        # Parameters that need to be passed to the get request
+        self.payload = {
+            'method': 'tag.gettoptags',
+            'api_key': self.API_KEY,
+            'format': 'json'
+        }
+        # Request to get a list of genres to choose from
+        self.r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=self.headers, params=self.payload)
+
+    def test_request_genre_status(self):
+        self.assertEqual(200,self.r.status_code)
+
+    def test_request_songs(self):
+        self.payload['method'] = 'tag.gettoptracks'
+        topSongs = requests.get(self.ROOT_URL, headers = self.headers, params = self.payload)
+        self.assertEqual(200,topSongs.status_code)
 
 if __name__ == '__main__':
     unittest.main()
