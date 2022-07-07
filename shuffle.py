98f851bb235d5b3a93864df98f21837aa3c94a34
Wadi Ahmed <wadiahmed2@gmail.com>
2022-07-07 19:40:56 +0000

    Revert "Edited README and added database functionality and querying"
    This reverts commit 116a2d4ba0d8e3ab25275cde0acddc4f4e1460c3.

--- shuffle.py
+++ shuffle.py
@@ -1,12 +1,5 @@
 import requests
-import sqlalchemy as db
-import pandas as pd
 import os
-
-def search_artist(artist):
-    
-    query = engine.execute(f"SELECT title FROM songs WHERE artist = '{artist}';").fetchall()
-    print(pd.DataFrame(query, columns=['Song Title']))
 
 # Constants to get api key from environment variables
 API_KEY = os.environ.get('API_KEY')
@@ -47,22 +40,6 @@ parameters = {
 # Request that gets the top songs that match the chosen genre
 topSongs = requests.get(ROOT_URL, headers=headers, params = parameters).json()
 playlist = []
-artists = []
 for song in topSongs['tracks']['track']:
     playlist.append(song['name'])
-    artists.append(song['artist']['name'])
 print(f'The playlist is: {playlist}')
-
-# Code to create the database of songs in the playlist
-df = pd.DataFrame({'title': playlist, 'artist': artists})
-engine = db.create_engine('sqlite:///playlists.db')
-df.to_sql('songs', con=engine, if_exists='replace')
-query_result = engine.execute("SELECT * FROM songs;").fetchall()
-print(pd.DataFrame(query_result, columns=['Number','Song Title','Artist']))
-
-# Code to query the playlist for songs by a certain artist
-to_query = input('Do you wish to query the playlist Y/N: ')
-if to_query.upper() != 'Y':
-    quit()
-artist = input('Please input an artist\'s name to search for: ')
-search_artist(artist)
